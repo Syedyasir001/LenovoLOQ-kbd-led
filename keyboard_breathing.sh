2921af49456b5f005ceaed998f6076fa08c87f68
Syed Yasir <yasironline54@gmail.com>
2025-03-14 21:02:33 +0530

    keyboard_breathing.sh

--- keyboard_breathing.sh
+++ keyboard_breathing.sh
@@ -3,7 +3,7 @@
 
 while true; do
     for i in {0..3}; do
-        echo $i | sudo tee $LED_PATH
+        echo $i | sudo tee $LED_PATH       #this is for keyboards with three brightness levels.
         sleep 0.2
     done
     for i in {3..0}; do
