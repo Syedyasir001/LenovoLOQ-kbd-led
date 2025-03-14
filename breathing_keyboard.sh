16657762e5916df0df7253e9c22e87f75d9afe54
Syed Yasir <yasironline54@gmail.com>
2025-03-14 22:08:37 +0530

    Delete breathing_keyboard.sh

--- breathing_keyboard.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-while true; do
-    for i in {0..2} {2..0}; do
-        echo $i | sudo tee /sys/class/leds/platform::kbd_backlight/brightness > /dev/null
-        sleep 0.1  # Adjust speed of the breathing effect
-    done
-done
