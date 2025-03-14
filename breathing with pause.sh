00e826d272351840350f084e894d199486992183
Syed Yasir <yasironline54@gmail.com>
2025-03-14 22:07:44 +0530

    Delete breathing with pause.sh

--- breathing with pause.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-  for i in 0 1 2 1 0; do
-    echo $i | sudo tee /sys/class/leds/platform::kbd_backlight/brightness > /dev/null
-    sleep 0.5  # Adjust this for speed of breathing
-  done
-  sleep 2  # <-- This adds a pause before the next cycle
-done
-
